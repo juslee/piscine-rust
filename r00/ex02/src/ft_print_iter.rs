7a83bc5fa236ab98d957f2ea9f65548f39a01c2c
Justin Lee <triplez@justinlee.sg>
2025-05-26 16:24:32 +0800

    chore: add documentation

--- r00/ex02/src/ft_print_iter.rs
+++ r00/ex02/src/ft_print_iter.rs
@@ -6,13 +6,17 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:37:00 by welee             #+#    #+#             */
-/*   Updated: 2025/05/26 15:53:18 by welee            ###   ########.fr       */
+/*   Updated: 2025/05/26 16:02:27 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 use std::io::Write;
 use crate::ft_putchar;
 
+/// Prints each character from the given iterator to the provided writer.
+/// This function takes a mutable reference to a writer and an iterator.
+/// It converts each item in the iterator into a character and prints it using `ft_putchar`.
+/// The function does not return any value.
 pub fn ft_print_iter<W, I>(mut w: W, iter: I)
 where
 	W: Write,
