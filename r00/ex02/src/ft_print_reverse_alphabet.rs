b354903386bcac3f01ea4af65f99b7f26487c704
Justin Lee <triplez@justinlee.sg>
2025-05-26 16:50:47 +0800

    doc: fix ft_print_reverse_alphabet

--- r00/ex02/src/ft_print_reverse_alphabet.rs
+++ r00/ex02/src/ft_print_reverse_alphabet.rs
@@ -6,19 +6,14 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 10:43:55 by welee             #+#    #+#             */
-/*   Updated: 2025/05/26 15:57:50 by welee            ###   ########.fr       */
+/*   Updated: 2025/05/26 16:39:49 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 use std::io::Write;
 use crate::ft_print_iter;
 
-/// Prints the lowercase alphabet from 'z' to 'a' to the standard output.
-/// This function uses the `ft_putchar` function to print each character.
-/// It iterates over the ASCII values of lowercase letters and converts them to characters.
-/// The `ft_putchar` function is called for each character to print it.
-/// The function does not return any value.
-/// It is designed to be used with any type that implements the `Write` trait,
+/// Prints the lowercase alphabet in reverse order to the provided writer.
 pub fn ft_print_reverse_alphabet<W: Write>(mut w: W) {
 	ft_print_iter::ft_print_iter(&mut w, (b'a'..=b'z').rev());
 }
